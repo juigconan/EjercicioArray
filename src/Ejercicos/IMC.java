ef611d812e46bc80e27e9a5a0d702e65844df7c7
juig_ <juig_@DESKTOP-0OPHGMB>
2022-11-23 23:12:14 +0100

    jejeje

--- src/Ejercicos/IMC.java
+++ src/Ejercicos/IMC.java
@@ -1,34 +1,77 @@
 package Ejercicos;
+
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class IMC {
-	
-	public static Sujeto crearSujeto() {
+
+	private static Sujeto crearSujeto() {
 		String nombre;
 		int peso;
 		int altura;
 		System.out.print("Introduce un nombre: ");
 		nombre = Entrada.cadena();
-		System.out.printf("%n Introduce un peso en Kg: ");
+		System.out.printf("Introduce un peso en Kg: ");
 		peso = Entrada.entero();
-		System.out.printf("%n Introduce una altura en cm: ");
+		System.out.printf("Introduce una altura en cm: ");
 		altura = Entrada.entero();
 		return new Sujeto(nombre, peso, altura);
+	}
+
+	private static double calcularMedia(Sujeto[] arraySujeto) {
+		double media = 0;
+		for (int i = 0; i < arraySujeto.length; i++) {
+			arraySujeto[i] = crearSujeto();
+			media += arraySujeto[i].getImc();
+		}
+
+		media /= arraySujeto.length;
+		return media;
 	}
 
 	public static void main(String[] args) {
 		Sujeto[] arraySujeto;
 		int cantidad;
+		double mayorImc = 0;
+		double menorImc = 0;
+		double media = 0;
+		int contEncimaMedia = 0;
+		int contDebajoMedia = 0;
+		Sujeto sujetoEncimaMedia = new Sujeto();
+		Sujeto sujetoDebajoMedia = new Sujeto();
+		sujetoEncimaMedia.setImc(0);
+		sujetoDebajoMedia.setImc(100);
+
 		do {
 			System.out.print("Sujetos a introducir (minimo 2): ");
 			cantidad = Entrada.entero();
 		} while (cantidad < 2);
-		
+
 		arraySujeto = new Sujeto[cantidad];
-		
+
+		media = calcularMedia(arraySujeto);
+
 		for (int i = 0; i < arraySujeto.length; i++) {
-			crearSujeto();
+			if (arraySujeto[i].getImc() < media) {
+				contDebajoMedia++;
+			}
+			// si es igual a la media consideramos que esta por debajo
+			if (arraySujeto[i].getImc() >= media) {
+				contEncimaMedia++;
+			}
+			if (arraySujeto[i].getImc() < sujetoDebajoMedia.getImc()) {
+				sujetoDebajoMedia = new Sujeto(arraySujeto[i]);
+				menorImc = sujetoDebajoMedia.getImc();
+			}
+			if (arraySujeto[i].getImc() > sujetoEncimaMedia.getImc()) {
+				sujetoEncimaMedia = new Sujeto(arraySujeto[i]);
+				mayorImc = sujetoEncimaMedia.getImc();
+			}
 		}
+
+		System.out.printf(
+				"Media: %2.2f%%, por encima de la media: %d, por debajo de la media: %d, mayorImc: %2.2f%%, menorImc %2.2f%%, nombre Mayor: %s, nombre menor: %s",
+				media, contEncimaMedia, contDebajoMedia, mayorImc, menorImc, sujetoEncimaMedia.getNombre(),
+				sujetoDebajoMedia.getNombre());
 	}
-	
+
 }
--- src/Ejercicos/Sujeto.java
+++ src/Ejercicos/Sujeto.java
@@ -2,4 +2,71 @@ package Ejercicos;
 
 public class Sujeto {
 
+	String nombre;
+	int peso;
+	int altura;
+	double imc;
+	
+	public Sujeto() {
+		setNombre("Nombre defecto");
+		setPeso(0);
+		setAltura(0);
+		setImc();
+	}
+	
+	public Sujeto(String nombre, int peso, int altura) {
+		setNombre(nombre);
+		setPeso(peso);
+		setAltura(altura);
+		setImc();
+	}
+	
+	public Sujeto(Sujeto sujeto) {
+		if(sujeto.equals(null)) {
+			throw new NullPointerException("ERROR: El sujeto no puede ser nulo");
+		}
+		setNombre(sujeto.getNombre());
+		setPeso(sujeto.getPeso());
+		setAltura(sujeto.getAltura());
+		setImc();
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public double getImc() {
+		return imc;
+	}
+
+	public void setImc() {
+		this.imc = 1000*(getPeso()/Math.pow(getAltura(),2));
+	}
+	
+	//Este es para poner un imc concreto aunque solo lo utilizaremos para cambiar los sujetos por defecto
+	public void setImc(int imc) {
+		this.imc = imc;
+	}
+	
 }
