68e234fecd42bf3006197adac00a15506bca38a4
juigconan <94482441+juigconan@users.noreply.github.com>
2022-11-30 13:58:09 +0100

    modificadas las expresiones regulares

--- src/Ejercicios/Variados/GestionClientes/DatosContacto.java
+++ src/Ejercicios/Variados/GestionClientes/DatosContacto.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class DatosContacto {
 
 	private static final String ER_TELEFONO = "\\d{9}";
-	private static final String ER_CORREO = "([a-zA-Z0-9_-])+@([a-zA-Z0-9-])+\\\\.([a-zA-Z])+";
+	private static final String ER_CORREO = "([a-zA-Z0-9_-\\.])+@([a-zA-Z0-9-])+\\.([a-zA-Z])+";
 	private DireccionPostal direccionPostal;
 	private String telefono;
 	private String correo;
